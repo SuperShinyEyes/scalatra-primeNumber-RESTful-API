c3891808dd7ce3861fb1a1f0cb503f9f36ed5a9f
Young Park <i.make.seyoung@gmail.com>
2016-02-06 17:06:01 +0200

    Write test NumberSpec with multiple intercepts

--- src/test/scala/NumberSpec.scala
+++ src/test/scala/NumberSpec.scala
@@ -4,7 +4,32 @@ import org.scalatest.Assertions._
 class NumberSpec extends FlatSpec {
     val one = new Number
     one.value = "1"
-    assertResult(true) {
-        one.value == 1
+    println(one.value)
+
+    "1" should "equal to Int(1)" in {
+        assertResult(true, "The number value is not right") {
+            one.value == 1
+        }
     }
+
+    it must "be prime" in {
+        assertResult(true) {
+            one.isPrime
+        }
+    }
+
+    "Number" must "be digits-only" in {
+        val number = new Number
+        intercept[java.io.IOException] {
+            number.value = "asdf"
+        }
+    }
+
+    "Number" must "have digits in right order" in {
+        val two = new Number
+        intercept[java.io.IOException] {
+            two.value = "01"
+        }
+    }
+
 }
