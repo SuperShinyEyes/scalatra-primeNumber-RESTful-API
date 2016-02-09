49f2932aa989b17d0d0ca7055b1acf3b9ae775eb
Young Park <i.make.seyoung@gmail.com>
2016-02-10 00:04:55 +0200

    Add comments in HelloServletSpec

--- src/test/scala/HelloServletSpec.scala
+++ src/test/scala/HelloServletSpec.scala
@@ -13,12 +13,15 @@ class HelloServletSpec extends MutableScalatraSpec {
         }
     }
 
+    /* Test valid inputs */
     testGet("/numbers=1,2,3,7,19,2141,5077&commands=removePrime", """{"numbers":""}""")
 
     testGet("numbers=1,1,1,3,4,5,6,99,300,3123&commands=inc,inc,inc,removePrime,removePrime,removePrime", "{\"numbers\":\"4, 6, 8, 9, 102, 303, 3126\"}")
 
     testGet("/numbers=3,6,23,5520,7041&commands=inc,removePrime,inc,removePrime", "{\"numbers\":\"25\"}")
 
+
+    /* Test invalid inputs */
     testGet("/numbers=&commands=removePrime", """{"error":"No numbers"}""")
 
     testGet("/numbers=1&commands=", """{"error":"No commands"}""")
