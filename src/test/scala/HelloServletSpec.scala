aef94e7d6296df03358d235bf936566b09df7c7b
Young Park <i.make.seyoung@gmail.com>
2016-02-10 00:02:19 +0200

    Apply DRY principle on HelloServletSpec

--- src/test/scala/HelloServletSpec.scala
+++ src/test/scala/HelloServletSpec.scala
@@ -3,39 +3,27 @@ import org.scalatra.test.specs2.MutableScalatraSpec
 class HelloServletSpec extends MutableScalatraSpec {
     addServlet(classOf[HelloServlet], "/*")
 
-    "Get /numbers=1,2,3,7,19,2141,5077&commands=removePrime" should {
-        "return {numbers:\"\"}" in {
-            get("/numbers=1,2,3,7,19,2141,5077&commands=removePrime") {
-                body must equalTo("""{"numbers":""}""")
+    private def testGet(params: String, reply: String) = {
+        s"Get $params" should {
+            s"return $reply" in {
+                get(params) {
+                    body must equalTo(s"""$reply""")
+                }
             }
         }
     }
 
-    "Get /numbers=1,1,1,3,4,5,6,99,300,3123&commands=inc,inc,inc,removePrime,removePrime,removePrime" should {
-        "return {\"numbers\":\"4, 6, 8, 9, 102, 303, 3126\"}" in {
-            val request2 = "numbers=1,1,1,3,4,5,6,99,300,3123&commands=inc,inc,inc,removePrime,removePrime,removePrime"
-            val reply2 = "{\"numbers\":\"4, 6, 8, 9, 102, 303, 3126\"}"
-            get(request2) {
-                body must equalTo("""%s""".format(reply2))
-            }
-        }
-    }
+    testGet("/numbers=1,2,3,7,19,2141,5077&commands=removePrime", """{"numbers":""}""")
 
-    "Get /numbers=3,6,23,5520,7041&commands=inc,removePrime,inc,removePrime" should {
-        "return {\"numbers\":\"25\"}" in {
-            val request3 = "/numbers=3,6,23,5520,7041&commands=inc,removePrime,inc,removePrime"
-            val reply3 = "{\"numbers\":\"25\"}"
-            get(request3) {
-                body must equalTo("""%s""".format(reply3))
-            }
-        }
-    }
+    testGet("numbers=1,1,1,3,4,5,6,99,300,3123&commands=inc,inc,inc,removePrime,removePrime,removePrime", "{\"numbers\":\"4, 6, 8, 9, 102, 303, 3126\"}")
 
-    "Get /numbers=&commands=removePrime" should {
-        "return {error: No numbers}" in {
-            get("/numbers=&commands=removePrime") {
-                body must equalTo("""{"error":"No numbers"}""")
-            }
-        }
-    }
+    testGet("/numbers=3,6,23,5520,7041&commands=inc,removePrime,inc,removePrime", "{\"numbers\":\"25\"}")
+
+    testGet("/numbers=&commands=removePrime", """{"error":"No numbers"}""")
+
+    testGet("/numbers=1&commands=", """{"error":"No commands"}""")
+
+    testGet("/numbers=1a&commands=inc", """{"error":"Invalid numbers: 1a. "}""")
+
+    testGet("/numbers=1&commands=incc", """{"error":"Invalid commands: incc."}""")
 }
