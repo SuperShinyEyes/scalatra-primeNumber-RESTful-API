7309556883d26b7f60b4a1bc2088e8f3a084962e
Young Park <i.make.seyoung@gmail.com>
2016-02-10 10:17:18 +0200

    Test negative/float inputs

--- src/test/scala/HelloServletSpec.scala
+++ src/test/scala/HelloServletSpec.scala
@@ -14,7 +14,7 @@ class HelloServletSpec extends MutableScalatraSpec {
     }
 
     /* Test valid inputs */
-    testGet("/numbers=1,2,3,7,19,2141,5077&commands=removePrime", """{"numbers":""}""")
+    testGet("/numbers=-7,-3,-2,0,1,2,3,7,19,2141,5077&commands=removePrime", """{"numbers":"-7, -3, -2, 0, 1"}""")
 
     testGet("numbers=1,1,1,3,4,5,6,99,300,3123&commands=inc,inc,inc,removePrime,removePrime,removePrime", "{\"numbers\":\"4, 6, 8, 9, 102, 303, 3126\"}")
 
@@ -23,14 +23,14 @@ class HelloServletSpec extends MutableScalatraSpec {
 
     /* Test number sanitization */
     testGet("/numbers=01,0002&commands=inc", """{"numbers":"2, 3"}""")
-    
+
 
     /* Test invalid inputs */
     testGet("/numbers=&commands=removePrime", """{"error":"No numbers"}""")
 
     testGet("/numbers=1&commands=", """{"error":"No commands"}""")
 
-    testGet("/numbers=1a&commands=inc", """{"error":"Invalid numbers: 1a. "}""")
+    testGet("/numbers=1.1,1a,-3.2&commands=inc", """{"error":"Invalid numbers: 1a, -3.2, 1.1. "}""")
 
     testGet("/numbers=1&commands=incc", """{"error":"Invalid commands: incc."}""")
 }
